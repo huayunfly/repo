1aea80ab3456534464b92cd4587ac44d92a47f35
Yun Hua <huayunfly@126.com>
2016-06-22 17:32:51 +0800

    add create_table, insert_row, query database functions

--- py/sqlitedb/imagemgr.py
+++ py/sqlitedb/imagemgr.py
@@ -1,6 +1,7 @@
 __author__ = 'yun_hua'
 
 import sqlite3
+import logging
 
 class ImageMgr(object):
     """Manage images with words relationship in database,
@@ -8,25 +9,87 @@ class ImageMgr(object):
     """
     def init_with_path(self, path):
         # assert(sqlite3_threadsafe)
-        self.__databasePath = path
+        self._databasePath = path
 
     def __init__(self):
-        self.__databasePath = None
-        self.__db = None
+        self._databasePath = None
+        self._conn = None
 
-    def get_sqlite_path(self):
-        if self.__databasePath is None:
-            return ":memory:"
-        else:
-            return self.__databasePath
+    @property
+    def sqlpath(self):
+        return self._databasePath
 
-    sqlitePath = property(get_sqlite_path)
+    @sqlpath.setter
+    def sqlpath(self, value):
+        if value is not None:
+            self._databasePath = value
+
+    @staticmethod
+    def sqlite3_version():
+        return sqlite3.sqlite_version
 
     def open(self):
-        conn = sqlite3.connect(self.sqlitePath)
-        if conn in not None:
+        """Open database using path"""
+        if self._conn is not None:
+            return True
+        self._conn = sqlite3.connect(self.sqlpath)
+        if self._conn is not None:
             return True
         else:
+            print "Opening database %r failed" % self._conn
+            logging.debug("Opening database %r failed", self._conn)
             return False
+
+    def close(self):
+        """Close the database
+        """
+        if self._conn is None:
+            return True
+
+        self._conn.close()
+        return True
+
+    def create_db_table(self, sql):
+        """sql e.g. "CREATE TABLE stocks
+             (date text, trans text, symbol text, qty real, price real)"
+        SQLite natively supports the following types: NULL, INTEGER, REAL, TEXT, BLOB.
+
+        The following Python types can thus be sent to SQLite without any problem:
+        Python type: SQLite type = {None: NULL, int: INTEGER, long: INTEGER, float: REAL,
+        str(UTF8): TEXT, unicode: TEXT, buffer: BLOB}
+
+        SQLite to Python:
+        {NULL: None, INTEGER: int or long, REAL: float, TEXT: depends on text_factory,
+        unicode by default, BLOB: buffer}
+        """
+        c = self._conn.cursor()
+        res = c.execute(sql)
+        if res is None:
+            return False
+        else:
+            return True
+
+    def insert_db_row(self, sql):
+        """Insert a row in db"""
+        c = self._conn.cursor()
+        c.execute(sql)
+        self._conn.commit()
+
+    def query_db(self, query, params):
+        """Query row, update db using sql.
+        Using ? as a placeholder to construct a secure query instead of using Python string operation.
+        e.g.
+        t = ('RHAT',)
+        c.execute('SELECT * FROM stocks WHERE symbol=?', t)
+
+        NEVER DO THIS:
+        symbol = 'RHAT'
+        c.execute("SELECT * FROM stocks WHERE symbol = '%s'" % symbol)
+        """
+        c = self._conn.cursor()
+        return c.execute(query, params)
+
+
+
 
 
--- py/test/imagemgrtest.py
+++ py/test/imagemgrtest.py
@@ -1,13 +1,35 @@
 __author__ = 'yun_hua'
 
 import unittest
-import sqlitedb
+import sqlite3
+from sqlitedb.imagemgr import ImageMgr
 
 
 class ImageMgrTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def setUp(self):
+        self.mgr = ImageMgr()
+        self.mgr.init_with_path(":memory:")
 
+    def tearDown(self):
+        self.mgr.close()
+
+    def test_db_open(self):
+        self.assertTrue(self.mgr.open())
+
+    def test_create_table(self):
+        sql = """CREATE TABLE stocks
+            (date text, trans text, symbol text, qty real, price real)"""
+        self.mgr.open()
+        self.assertTrue(self.mgr.create_db_table(sql))
+        sql = """INSERT INTO stocks VALUES ('2016-06-22', "BUY", "RHAT", 100, 34.5) """
+        self.mgr.insert_db_row(sql)
+        queryError = """SELECT * FROM stocks WHERE symbolsss=?"""
+        params = ("RHAT",)
+        self.assertRaises(sqlite3.OperationalError, self.mgr.query_db(queryError, params))
+
+        query = """SELECT * FROM stocks WHERE symbol=?"""
+        for row in self.mgr.query_db(query, params):
+            print row
 
 if __name__ == '__main__':
     unittest.main()
