15f5a5f46524641cfee42c6d56a5baf8619970d6
yun_hua <hello@123>
2016-06-27 17:31:24 +0800

    modify test_create_update to add a words database

--- py/test/imagemgrtest.py
+++ py/test/imagemgrtest.py
@@ -1,3 +1,4 @@
+# _*_ coding: utf-8 _*_
 __author__ = 'yun_hua'
 
 import unittest
@@ -8,7 +9,7 @@ from sqlitedb.imagemgr import ImageMgr
 class ImageMgrTestCase(unittest.TestCase):
     def setUp(self):
         self.mgr = ImageMgr()
-        self.mgr.init_with_path(":memory")
+        self.mgr.init_with_path(":memory:")
         try:
             self.mgr.open()
         except sqlite3.OperationalError, err:
@@ -21,16 +22,16 @@ class ImageMgrTestCase(unittest.TestCase):
         self.assertTrue(self.mgr.open())
 
     def test_create_update(self):
-        sql = """CREATE TABLE stocks
-            (date text, trans text, symbol text, qty real, price real)"""
+        sql = """CREATE TABLE english_lib
+            (word text, us_phonetic text, uk_phonetic text, chs text, chs_phonetic text, category integer)"""
         self.assertTrue(self.mgr.create_db_table(sql))
-        sql = """INSERT INTO stocks VALUES ('2016-06-22', "BUY", "RHAT", 100, 34.5) """
+        sql = """INSERT INTO english_lib VALUES ("star", "[stɑr]", "[stɑː(r)]", "星星", "xingxing", 1) """
         self.mgr.insert_db_row(sql)
-        queryError = """SELECT * FROM stocks WHERE symbolsss=?"""
-        params = ("RHAT",)
-        self.assertRaises(sqlite3.OperationalError, self.mgr.query_db(queryError, params))
+        # queryError = """SELECT * FROM english_lib WHERE symbolsss=?"""
+        params = (u"star",)
+        # self.assertRaises(sqlite3.OperationalError, self.mgr.query_db(queryError, params))
 
-        query = """SELECT * FROM stocks WHERE symbol=?"""
+        query = """SELECT * FROM english_lib WHERE word=?"""
         for row in self.mgr.query_db(query, params):
             print row
 
