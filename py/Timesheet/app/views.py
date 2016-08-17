48dcd815e2b593015c3c9d4a3996d911d44693fc
huayunfly <cuiguoxia@sohu.com>
2016-08-17 09:07:47 +0800

    select the working day for the timeline

--- py/Timesheet/app/views.py
+++ py/Timesheet/app/views.py
@@ -6,9 +6,12 @@ from django.shortcuts import render
 from django.http import HttpRequest
 from django.template import RequestContext
 from datetime import datetime
+from datetime import timedelta
 from app.models import TaskTime
 from app.models import Project
 from app.models import Person
+
+WEEK_DAYS_NUM = 7
 
 
 def home(request):
@@ -61,8 +64,24 @@ def timeline(request, year, month, week=0):
         weeknumber = -1
     else:
         weeknumber -= 1
-    monday = datetime(int(year), int(month), weeks[weeknumber][0])
-    sunday = datetime(int(year), int(month), weeks[weeknumber][-1])
+    first = 0
+    # end = 0
+    # Find the first day in one week of this month like [0, 0, 0, 0, 1, 2, 3]
+    for day in range(0, WEEK_DAYS_NUM, 1):
+        if 0 != weeks[weeknumber][day]:
+            first = day
+            break
+    # Find the last day in one week of this month like [26, 27, 28, 29, 30, 31, 0]
+    #for day in range(6, -1, -1):
+    #    if 0 != weeks[weeknumber][day]:
+    #        end = day
+    #        break
+
+    # Make a valid week range which may cover the last month or the next month.
+    monday = datetime(int(year), int(month), weeks[weeknumber][first]) - timedelta(days=first)
+    weekdays = [monday]
+    for day in range(1, WEEK_DAYS_NUM, 1):
+        weekdays.append(monday + timedelta(days=day))
 
     return render(
         request,
@@ -72,8 +91,9 @@ def timeline(request, year, month, week=0):
             'message': 'Your timeline page',
             'year': datetime.now().year,
             'tasks': TaskTime.objects.filter(employee__user__username=request.user.username,
-                                             workday__gte=monday,
-                                             workday__lte=sunday),
+                                             workday__gte=weekdays[0],
+                                             workday__lte=weekdays[-1]),
             'projects': Project.objects.all(),
+            'weekdays': weekdays,
         }
     )
